e9d9c55a91fa70f3d516c5930ad42231d1959d3c
iftoute <peinoit.jessie@orange.fr>
2021-06-15 16:44:51 +0200

    Modification src promise product.js

--- js/product.js
+++ js/product.js
@@ -17,15 +17,15 @@ fetch (url)
             }
         })
         // Promise pour les éléments reçus par le serveur de l'objet sélectionné
-        .then(cameramais  => {
+        .then(cameras  => {
             let product = document.getElementById('product');
             // Insertion d'élements HTML pour l'article sélectionné
             product.insertAdjacentHTML('beforeend', `<a href="produit.html" class="col col-sm-12 col-md-6 col-lg-4 mb-4">
                                                     <div id= "article" class="text-center">
-                                                        <img class="imageCamera shadow mb-3" src="${id.imageUrl}" width="100%" height="200" alt="image camera">
-                                                        <h3 class= "nameCamera">${id.name}</h3>
-                                                        <p id="descriptionCamera" class="col 4">${id.description}</p>
-                                                        <p class="priceCamera">${(id.price / 100).toFixed(2)}€</p>
+                                                        <img class="imageCamera shadow mb-3" src="${camera.imageUrl}" width="100%" height="200" alt="image camera">
+                                                        <h3 class= "nameCamera">${camera.name}</h3>
+                                                        <p id="descriptionCamera" class="col 4">${camera.description}</p>
+                                                        <p class="priceCamera">${(camera.price / 100).toFixed(2)}€</p>
                                                         </a>`
             );
            
