9adc343a0834787b0dc96b7eec98719f87897a50
iftoute <peinoit.jessie@orange.fr>
2021-06-24 15:41:20 +0200

    Add functions basket

--- js/basket.js
+++ js/basket.js
@@ -8,37 +8,36 @@ let contact =
 };
 let total = 0;
 
+//on récupère les éléments contenus dans le panier
+let basketContent = JSON.parse(localStorage.getItem('basket'));
+//console.log(basketContent);
 
-
-
-//FONCTION AFFICHER LE PANIER
-function affichePanier() {
-    for (let element of storagePanier) {
-        if (element.quantite > 0) {
-            let contentPanier = document.getElementById("eltPanier");
-            contentPanier.insertAdjacentHTML('beforeend', `<div class="col-6">
-                                                            <img class="imageCamera shadow mb-3" src="${camera.imageUrl}" width="100" height="100" alt="image camera">
-                                                        </div>
-                                                        <div class="col-6">
-                                                            <h3 class= "nameCamera my-4">${camera.name}</h3>
-                                                            <input type="number" id="${camera._id}" min="0" value="${camera.quantite}" onchange="changePrice(id)">
-                                                            <p id="total${camera._id}">${(camera.price * camera.quantite / 100).toFixed(2)}€</p>
-                                                            <button id="${camera._id}" onclick="deleteProduct(id)">X</button>
-                                                        </div>
-                                                                `)
-
-            //total += (camera.price * camera.quantite / 100);
-        }
+//si le panier est vide
+if(basketContent === null){
+    let emptyBasket = document.getElementById('eltPanier');
+    emptyBasket.insertAdjacentHTML('beforeend', `<div class="col-6">
+                                                    <h3 class="text-center my-5">Votre panier est vide</h3>
+                                                </div>
+                                                `)
+//si le panier est n'est pas vide, afficher tous les produits du localStorage
+}else {
+    for(let camera of basketContent){
+    let eltBasket = document.getElementById('eltPanier');
+    //VOIR COMMENT AFFICHER LES ELEMENTS DU PRODUIT GRACE A SON ID
+    eltBasket.insertAdjacentHTML('beforeend',  ` <div class="col-6">
+                                                        <img class="imageCamera shadow mb-3" src="${camera.imageUrl}" width="100" height="100" alt="image camera">
+                                                    </div>
+                                                    <div class="col-6">
+                                                        <h3 class= "nameCamera my-4">${camera.name}</h3>
+                                                        <input type="number" id="${camera._id}" min="0" value="${camera.quantite}" onchange="changePrice(id)">
+                                                        <p id="total${camera._id}">${(camera.price * camera.quantite / 100).toFixed(2)}€</p>
+                                                        <button id="${camera._id}" onclick="deleteProduct(id)">X</button>
+                                                    </div>
+                                                    `)
+        total += (camera.price * camera.quantite / 100);
+        console.log(total);
     }
 };
-/////////////////////////////////////////////////////////////////
-function savePanier(panier){
-    localStorage.setItem('panier', JSON.stringify(panier));
-}
-//////////////////////////////////////////////////////////////////
-function removePanier(product){
-
-}
 
 
 
