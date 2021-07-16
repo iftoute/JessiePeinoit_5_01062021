63798490ff86aa42e42032c30191d18c8a296221
iftoute <peinoit.jessie@orange.fr>
2021-07-16 19:07:25 +0200

    Media queries product

--- js/basket.js
+++ js/basket.js
@@ -1,39 +1,34 @@
-//on récupère les éléments contenus dans le panier
+// on récupère les éléments contenus dans le panier
 let basketContent = JSON.parse(localStorage.getItem('basket'));
-//console.log('basketContent:',JSON.parse(basketContent));
+
+// création de la variable qui va stocker le total du panier
+let total = 0;
+
 //si le panier est vide
-if(basketContent === null){
-    let emptyBasket = document.getElementById('eltPanier');
-    emptyBasket.insertAdjacentHTML('beforeend', `<div class="col-12">
+if (basketContent === null) {
+  let emptyBasket = document.getElementById('eltPanier');
+  emptyBasket.insertAdjacentHTML('beforeend', `<div class="col-12">
                                                     <h3 class="text-center my-5">Votre panier est vide</h3>
                                                 </div>
                                                 `)
 //si le panier est n'est pas vide, afficher tous les produits du localStorage
-}else {
-    var total = 0;
-    for(let cameraId of basketContent){
-        let url = "http://localhost:3000/api/cameras/" + cameraId;
-        fetch (url)
-        .then(function (response) {
-            if (response.ok) {
-                return response.json()
-            } else {
-                console.log("Problème de connexion au serveur")
-            }
-        })
-        .then(camera => {
-            let eltBasket = document.getElementById('eltPanier');
-            eltBasket.insertAdjacentHTML('beforeend',  ` <div class="eltFromBasket">
-                                                            <div class="row text-center">
-                                                                <img class="imageCamera col-3 mx-auto shadow-lg mb-3 px-0" src="${camera.imageUrl}" width="100" height="100" alt="image camera">
-                                                            </div>
-                                                            <div class="row text-center">
-                                                                <h4 class="nameCamera my-1">${camera.name}</h4>
-                                                                <p id="price" class="mb-5">${(camera.price/100).toFixed(2)}€</p>
-                                                            </div>
-                                                         </div>
-                                                            `)
-                // calcul du total
+} else {
+  for (let cameraId of basketContent){
+    const url = `http://localhost:3000/api/cameras/${cameraId}`;
+      fetch (url)
+      .then((response) => response.json())
+      .then(camera => {
+        let eltBasket = document.getElementById('eltPanier');
+        eltBasket.insertAdjacentHTML('beforeend', `<div class="eltFromBasket">
+                                                      <div class="row text-center">
+                                                        <img class="imageCamera col-3 mx-auto shadow-lg mb-3 px-0" src="${camera.imageUrl}" width="100" height="100" alt="image camera">
+                                                      </div>
+                                                      <div class="row text-center">
+                                                        <h4 class="nameCamera my-1">${camera.name}</h4>
+                                                        <p id="price" class="mb-5">${(camera.price/100).toFixed(2)}€</p>
+                                                      </div>
+                                                    </div>`)
+        // calcul du total
         total += camera.price / 100;
         const totalContent = document.getElementById('total');
         totalContent.innerHTML = `Total de la commande : ${total.toFixed(2)} €`;
@@ -43,66 +38,67 @@ if(basketContent === null){
       .catch((error) => alert(`Erreur : ${error}`));
   }
 
-  // Stockage des produits du panier dans la variable products attendue par l'API
-  const products = basketContent;
+// Stockage des produits du panier dans la variable products attendue par l'API
+const products = basketContent;
 
-  // Création de la class concernant les informations du formulaire
-  class FormContact {
-    constructor(lastName, firstName, email, address, city) {
-      this.lastName = lastName;
-      this.firstName = firstName;
-      this.email = email;
-      this.address = address;
-      this.city = city;
-    }
+// Création de la class concernant les informations du formulaire
+class FormContact {
+  constructor(lastName, firstName, email, address, city) {
+    this.lastName = lastName;
+    this.firstName = firstName;
+    this.email = email;
+    this.address = address;
+    this.city = city;
   }
+}
 
-  // Ajout des informations du formulaire à la variable contact
-  const contact = new FormContact(lastName, firstName, email, address, city);
+// Ajout des informations du formulaire à la variable contact
+const contact = new FormContact(lastName, firstName, email, address, city);
 
-  // Fonction qui vérifié la validité des éléments du formulaire et envoie la commande à l'API
-  function submitOrder() {
-    for (let input of document.querySelectorAll('#form > input:not([type="submit"])')){ 
-      input.reportValidity();
-    }
+// Fonction qui vérifié la validité des éléments du formulaire et envoie la commande à l'API
+function submitOrder() {
+  for (let input of document.querySelectorAll('#form > input:not([type="submit"])')){ 
+    input.reportValidity();
   }
+}
 
-  // Création de la variable qui va stocker les éléments de la commande (contact + products)
-  const order = {
-    contact,
-    products,
+// Création de la variable qui va stocker les éléments de la commande (contact + products)
+const order = {
+  contact,
+  products,
+};
+
+// fonction POST de la commande à l'API
+function sendOrder () {
+  const url = 'http://localhost:3000/api/cameras/order';
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(order),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
   };
-
-  // fonction POST de la commande à l'API
-  function sendOrder () {
-    const url = 'http://localhost:3000/api/cameras/order';
-    const options = {
-      method: 'POST',
-      body: JSON.stringify(order),
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    };
-    fetch(url, options)
-      .then((response) => response.json())
-      .then((json) => {
-        localStorage.setItem('orderId', json.orderId);
-        // redirection vers la page de confirmation de commande
-        window.location.href = 'http://127.0.0.1:5500/html/confirmation.html';
-      })
-      .catch((error) => alert(`Erreur : ${error}`));
+    
+  fetch(url, options)
+    .then((response) => response.json())
+    .then((json) => {
+      localStorage.setItem('orderId', json.orderId);
+      // redirection vers la page de confirmation de commande
+      window.location.href = 'http://127.0.0.1:5500/html/confirmation.html';
+    })
+    .catch((error) => alert(`Erreur : ${error}`));
   }
 
-  // Fonction appelée à la soummission du formulaire
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    contact.lastName = lastName.value;
-    contact.firstName = firstName.value;
-    contact.address = address.value;
-    contact.city = city.value;
-    contact.email = email.value;
-    submitOrder();
-    sendOrder();
-  });
+ // Fonction appelée à la soummission du formulaire
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  contact.lastName = lastName.value;
+  contact.firstName = firstName.value;
+  contact.address = address.value;
+  contact.city = city.value;
+  contact.email = email.value;
+  submitOrder();
+  sendOrder();
+});
 }
--- js/product.js
+++ js/product.js
@@ -18,19 +18,17 @@ fetch(url)
         let product = document.getElementById('product');
 
         // Insertion d'élements HTML pour l'article sélectionné
-        product.insertAdjacentHTML('beforeend', `   <div class="col-6">
+        product.insertAdjacentHTML('beforeend', `   <div class="col-12 col-md-6">
                                                         <img class="imageCamera shadow mb-3" src="${camera.imageUrl}" width="100%" height="400" alt="image camera">
                                                     </div>
-                                                    <div class="col-6">
+                                                    <div class="col-12 col-md-6">
                                                         <h2 class= "nameCamera my-4">${camera.name}</h2>
                                                         <p id="descriptionCamera" class="col text-justify">${camera.description}</p>
                                                         <p class="priceCamera fs-4 fw-bold">${(camera.price / 100).toFixed(2)}€</p>
                                                         <form class="form-group" id="form">
                                                             <label for="selectLens" class="col-4 my-3">Objectif</label>
                                                             <select name="productLens" class="col-7" id="selectLens"></select>
-                                                            <label for="quantité" class="col-4 mb-5" value="1">Quantité</label>
-                                                            <input type="number" name="howmuch" step="1" class="col-1 text-center" id="quantité" min="1" value="1">
-                                                            <button type="submit" id="addBasket" class="btn bg-dark bg-gradient text-white" onclick="addToBasket('${camera._id}')">Ajouter au panier</button>
+                                                            <button type="submit" id="addBasket" class="btn bg-dark bg-gradient text-white mt-5" onclick="addToBasket('${camera._id}')">Ajouter au panier</button>
                                                         </form>
                                                     </div>
                                                     `
